d09474d7211a221cd886585ef2532460f8ee9da3
raj-bunsha <48752322+raj-bunsha@users.noreply.github.com>
2020-10-30 08:55:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from cx_Freeze import setup, Executable 
 import sys
+#not for anything with program just built to freeze the code so that non-programmers can run
 #buildOptions = dict(include_files = [(absolute_path_to_your_file,'final_filename')]) #single file, absolute path.
 
 buildOptions = dict(include_files=['assets','examples','color_palettes.py','Basic-Regular.ttf']) #folder,relative path. Use tuple like in the single file to set a absolute path.

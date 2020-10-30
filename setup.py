192944ed4086a2936056e9533dfdfb93231816b7
raj-bunsha <48752322+raj-bunsha@users.noreply.github.com>
2020-10-30 08:53:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable 
+import sys
+#buildOptions = dict(include_files = [(absolute_path_to_your_file,'final_filename')]) #single file, absolute path.
+
+buildOptions = dict(include_files=['assets','examples','color_palettes.py','Basic-Regular.ttf']) #folder,relative path. Use tuple like in the single file to set a absolute path.
+
+executables = [
+    Executable('art_generator.py'),
+]
+setup(
+         name = "abstract_art_generator",
+         version = "1.0",
+         description = "description",
+         author = "raj",
+         options = dict(build_exe = buildOptions),
+         executables = executables)
